6fe1949b9b1eb1fac7afa8664c087d98e4e63abe
hsiao-yuan <yuantseng22923@gmail.com>
2024-04-01 16:43:35 +0800

    衝突測試

--- src/main/java/HelloWorldCinema/model/Menber.java
+++ src/main/java/HelloWorldCinema/model/Menber.java
@@ -3,5 +3,9 @@ package HelloWorldCinema.model;
 public class Menber {
 	public static void main(String[] args) {
 		System.out.println("HI!!!!!!");
+		System.out.println("1!!!!!!");
+		System.out.println("2!!!!!!");
+		System.out.println("3!!!!!!");
+		System.out.println("4!!!!!!");
 	}
 }
